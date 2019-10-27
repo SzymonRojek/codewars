7d8c45245dcf035b2436076e5d6f1e5bd302e675
Szymon Rojek <sz.rojek@gmail.com>
2019-10-27 22:24:34 +0000

    update 8-kyu make upper case - example 3

--- 8-kyu/makeUpperCase.js
+++ 8-kyu/makeUpperCase.js
@@ -18,3 +18,19 @@ makeUpperCase('i am learning js');
 const makeUpperCase = (s) => s.toUpperCase();
 
 makeUpperCase('hi');
+
+
+3.
+function makeUpperCase(str) {
+
+  let answer = '';
+
+  for(let i = 0; i < str.length; i++){
+    
+    console.log(str[i]);
+    answer += str[i].toUpperCase();
+  }
+  return answer;
+}
+
+console.log(makeUpperCase('this is another example'));
