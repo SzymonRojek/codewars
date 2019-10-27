458bea7e649721cbd9b3f86adecd0866715f9c1d
Szymon Rojek <sz.rojek@gmail.com>
2019-10-27 22:13:32 +0000

    update 8-kyu make upper case

--- 8-kyu/makeUpperCase.js
+++ 8-kyu/makeUpperCase.js
@@ -4,8 +4,17 @@
 
 // Write a function makeUpperCase:
 
+
+
 1.
+function makeUpperCase(str) {
+  return  str.toUpperCase();
+}
+
+makeUpperCase('i am learning js');
+
+
+2.
 const makeUpperCase = (s) => s.toUpperCase();
 
 makeUpperCase('hi');
-
