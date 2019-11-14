b7c313386007a3d41a66fe8c2f53ea04733a2fa6
Szymon Rojek <sz.rojek@gmail.com>
2019-11-14 23:08:35 +0000

    added 2 solution - removeStringSpaces

--- 8-kyu/removeStringSpaces.js
+++ 8-kyu/removeStringSpaces.js
@@ -8,4 +8,15 @@ function noSpace(x){
   }
 
 
-  
+
+// 2
+
+let x = '8aaaaa dddd r     ';
+
+let newX = x.split(' ');
+
+console.log(newX); 
+// ["8aaaaa", "dddd", "r", "", "", "", "", ""]
+
+let newX1 = newX.join('');
+console.log(newX1); //8aaaaaddddr
