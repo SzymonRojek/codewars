15cd8b32de6696c5f8cda4e60bb40f2acc1f53b6
Szymon Rojek <sz.rojek@gmail.com>
2019-11-15 14:03:43 +0000

    created file reversedStrings.js and added 1 solution

---
+++ 8-kyu/reversedStrings.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it reverses the string value passed into it:
+
+
+// 1
+
+const solution = x => x.split('').reverse().join('');
+
+console.log(solution('world')) // 'dlrow'
