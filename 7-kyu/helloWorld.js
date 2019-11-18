9074a56165859f0ae51897163317ef7c15e0c246
Szymon Rojek <sz.rojek@gmail.com>
2019-11-18 23:44:47 +0000

    added helloWorld.js and 1 solution

---
+++ 7-kyu/helloWorld.js
@@ -0,0 +1,12 @@
+
+// Given an argument that may be a String or a Number, return a simple greeting.
+
+// 1
+
+const hello = name => {
+  let greetings = 'Hello';
+   
+   return greetings + ' ' + name
+}
+
+console.log(hello('world')); // Hello world
