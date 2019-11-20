aa326d9d2d9e29b2d4a412f2d053b3ccf9e89a7b
Szymon Rojek <sz.rojek@gmail.com>
2019-11-20 12:05:32 +0000

    added 2 solution to the file helloWorld-withoutString.js

--- 7-kyu/helloWorld-withoutString.js
+++ 7-kyu/helloWorld-withoutString.js
@@ -29,3 +29,24 @@ check full list:
 http://www.fileformat.info/info/charset/UTF-16/list.htm
 
 */
+
+
+
+// 2
+
+const helloWorld = () => {
+  let d = String.fromCharCode(100);
+  let l = String.fromCharCode(108);
+  let e = String.fromCharCode(101);
+  let r = String.fromCharCode(114);
+  let H = String.fromCharCode(72);
+  let W = String.fromCharCode(87);
+  let o = String.fromCharCode(111);
+  let comma = String.fromCharCode(44);
+  let blank = String.fromCharCode(32);
+  let exclamation = String.fromCharCode(33);
+  
+  return H + e + l + l + o + comma + blank + W + o + r + l + d + exclamation;
+};
+
+console.log(helloWorld()); // Hello, World!
