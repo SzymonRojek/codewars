885d74126743e6cbccab2642810170436926e6b6
Szymon Rojek <sz.rojek@gmail.com>
2019-11-20 12:48:40 +0000

    created file isTheStringUppercase.js in the dir 8-kyu

---
+++ 8-kyu/isTheStringUppercase.js
@@ -0,0 +1,15 @@
+
+/*  Is the string uppercase?
+Add the isUpperCase method to String to see whether the string is ALL CAPS  */
+
+// 1
+
+
+String.prototype.isUpperCase = function() {
+  return this.valueOf().toUpperCase() === this.valueOf();
+};
+
+console.log("Hello".isUpperCase()); // false
+console.log("HELLO".isUpperCase()); // true
+
+
