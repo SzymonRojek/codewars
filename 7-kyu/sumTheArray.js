fece3f8b469a7e71b86b8f20c0f833d94cd59c8d
Szymon Rojek <sz.rojek@gmail.com>
2019-11-21 13:53:53 +0000

    created the file sumTheArray.js in the dir 7-kyu

---
+++ 7-kyu/sumTheArray.js
@@ -0,0 +1,11 @@
+
+
+/*  Sum the array:
+
+Add a method 'sum' to all array objects, which sums all the numbers in it. You should return 0 if the array is empty:
+
+let arr = [1,2,3,4];
+arr.sum();
+
+let arr = [10,28,14,33];
+arr.sum();  */ 
