16bb6c9f5728c13b258b9b6ad1129eb23c732985
Szymon Rojek <sz.rojek@gmail.com>
2019-11-29 22:58:16 +0000

    added 1 solution in the file descendingOrder.js

---
+++ 7-kyu/descendingOrder.js
@@ -0,0 +1,23 @@
+
+
+
+
+// change number to descending order (remember return number):
+
+
+//  1
+
+const descendingOrder = n => {
+  let arr = [...n + ""].sort((a, b) => b - a).join('');
+  
+  let convertToNumber = Number(arr);
+  
+  return convertToNumber;
+}
+
+console.log(descendingOrder(103245)); // 543210
+
+/* [...n + ""] - it means: take all digits from n and change them to strings */
+
+
+
