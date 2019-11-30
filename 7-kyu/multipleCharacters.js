3462a09bf41bcfd86eb7cb98acb2737f5df8341c
Szymon Rojek <sz.rojek@gmail.com>
2019-11-30 21:04:11 +0000

    added 1 solution in the file  multipleCharacters.js

--- 7-kyu/multipleCharacters.js
+++ 7-kyu/multipleCharacters.js
@@ -6,3 +6,22 @@ Expected results:
 spam(1);  // hue
 spam(6);  // huehuehuehuehuehue
 spam(14); // huehuehuehuehuehuehuehuehuehuehuehuehuehue */
+
+
+
+//  1
+
+const spam = number => {
+
+  var repeatedString = "";
+  let string = 'hue';
+
+  while (number > 0) {
+    repeatedString += string;
+    number--;
+  }
+
+  return repeatedString;
+
+}
+console.log(spam(2)); // huehue
