b58716037f95007da035ab14af08568b89578374
Szymon Rojek <sz.rojek@gmail.com>
2019-12-20 23:45:13 +0000

    added 2 solution in the 7-kyu/oddOrEven.js

--- 7-kyu/oddOrEven.js
+++ 7-kyu/oddOrEven.js
@@ -32,3 +32,21 @@ const oddOrEven = x => {
 };
 
 console.log(oddOrEven([1, 2])); // odd
+
+
+
+//  2
+
+const oddOrEven = x => {
+  let sum = 0;
+
+  for (let i = 0; i < x.length; i++) {
+          sum += x[i];
+  }
+
+  const result = sum % 2 ? 'odd' : 'even';
+
+  return result;
+};
+
+console.log(oddOrEven([1, 2, 3, 4])); // even
