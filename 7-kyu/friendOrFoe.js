e0e43dc6628eae82d83abacf3c84d5e6e04a4c5d
Szymon Rojek <sz.rojek@gmail.com>
2019-12-23 14:00:01 +0000

    modified the soluion nr 1 in the 7-kyu/friendOrFoe

--- 7-kyu/friendOrFoe.js
+++ 7-kyu/friendOrFoe.js
@@ -1,44 +1,33 @@
 
-
-
 /*
 Description:
 Make a program that filters a list of strings and returns a list 
 with only your friends name in it.
-
 If a name has exactly 4 letters in it, you can be sure that it 
 has to be a friend of yours! Otherwise, you can be sure he's not...
-
 Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
-
 i.e.
-
 friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
-
 Note: keep the original order of the names in the output.
 */
 
 
+
 //  1
 
-const friend = friends => {
-  // place to store myFriends;
+const whoIsaRealFriend = allFriends => {
+  // place to store realFriends
+  const realFriends = [];
 
-  const myFriends = [];
-
-  // iterate over the friends;
-
-  for (let i = 0; i < friends.length; i++) {
-          const friend = friends[i];
-
+  // iterate over the allFriends
+  for (let i = 0; i < allFriends.length; i++) {
           // if the friend name is length 4
-          if (friend.length === 4) {
-                  // push into my friends
-                  myFriends.push(friend);
+          if (allFriends[i].length === 4) {
+                  // push into my realFriend
+                  realFriends.push(allFriends[i]);
           }
   }
-
-  return myFriends;
+  return realFriends;
 };
 
-console.log(friend(['Ryan', 'Kieran', 'Mark'])); //  ["Ryan", "Mark"] 
+console.log(whoIsaRealFriend(['Ryan', 'Kieran', 'Mark']));
