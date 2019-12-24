b70961cfdb50cb4e0ff4536c55d8a1dc20cd725b
Szymon Rojek <sz.rojek@gmail.com>
2019-12-24 15:45:53 +0000

    added 2 solution in the file in 8-kyu/character-recognition-mistakes.js

--- 8-kyu/character-recognition-mistakes.js
+++ 8-kyu/character-recognition-mistakes.js
@@ -29,3 +29,23 @@ console.log(correct('BUDAPE5T')); // BUDAPEST
 console.log(correct('PAR15')); // PARIS
 
 
+
+//  2 I am using regular expression
+
+const correct = string => {
+  string = string.replace(/5/g, 'S');
+  string = string.replace(/0/g, 'O');
+  string = string.replace(/1/g, 'I');
+
+  // /5/g it means when you find the digit 5 globally
+  return string;
+};
+
+console.log(correct('L0ND0N')); // LONDON
+console.log(correct('DUBL1N')); // DUBLIN
+console.log(correct('51NGAP0RE')); // SINGAPORE
+console.log(correct('BUDAPE5T')); // BUDAPEST
+console.log(correct('PAR15')); // PARIS
+
+
+
