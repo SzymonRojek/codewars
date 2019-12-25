3dacf80ff087c4e7273ea4fc7183a41c04c521df
Szymon Rojek <sz.rojek@gmail.com>
2019-12-25 22:20:34 +0000

    added 7 solution in the file in 8-kyu/character-recognition-mistakes.js

--- 8-kyu/character-recognition-mistakes.js
+++ 8-kyu/character-recognition-mistakes.js
@@ -124,3 +124,19 @@ console.log(correct('DUBL1N')); // DUBLIN
 console.log(correct('51NGAP0RE')); // SINGAPORE
 console.log(correct('BUDAPE5T')); // BUDAPEST
 console.log(correct('PAR15')); // PARIS
+
+
+
+// 7 
+
+const correct = string =>
+        string
+                .replace(/0/g, 'O')
+                .replace(/5/g, 'S')
+                .replace(/1/g, 'I');
+
+console.log(correct('L0ND0N')); // LONDON
+console.log(correct('DUBL1N')); // DUBLIN
+console.log(correct('51NGAP0RE')); // SINGAPORE
+console.log(correct('BUDAPE5T')); // BUDAPEST
+console.log(correct('PAR15')); // PARIS
