bb840674e5fd8d79483a05862c9fa8eabd8acf22
Szymon Rojek <sz.rojek@gmail.com>
2019-12-25 19:52:35 +0000

    added 3 solution in the file in 8-kyu/character-recognition-mistakes.js

--- 8-kyu/character-recognition-mistakes.js
+++ 8-kyu/character-recognition-mistakes.js
@@ -1,9 +1,12 @@
 /*  Description:
-Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+Character recognition software is widely used to digitise printed texts. 
+Thus the texts can be edited, searched and stored on a computer.
 
-When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+When documents (especially pretty old ones written with a typewriter), 
+are digitised character recognition softwares often make mistakes.
 
-Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+Your task is correct the errors in the digitised text. You only have to 
+handle the following mistakes:
 
 S is misinterpreted as 5
 O is misinterpreted as 0
@@ -11,7 +14,8 @@ I is misinterpreted as 1
 The test cases contain numbers only by mistake. */
 
 
-//  1
+
+//  1 solutnion with .replace()
 
 const correct = string => {
   for (let i = 0; i < string.length; i++) {
@@ -30,11 +34,28 @@ console.log(correct('PAR15')); // PARIS
 
 
 
-//  2 I am using regular expression
+//  2 solution with .replace() and regular expression
+
+const correct = string => {
+  string = string.replace(/0/g, 'O');
+  string = string.replace(/5/g, 'S');
+  string = string.replace(/1/g, 'I');
+  return string;
+};
+
+console.log(correct('L0ND0N')); // LONDON
+console.log(correct('DUBL1N')); // DUBLIN
+console.log(correct('51NGAP0RE')); // SINGAPORE
+console.log(correct('BUDAPE5T')); // BUDAPEST
+console.log(correct('PAR15')); // PARIS
+
+
+
+//  3 solution with .replace(), regular expression, switch
 
 const correct = string =>
         string.replace(/5|0|1/g, letter => {
-          
+
                 switch (letter) {
                         case '5':
                                 return 'S';
