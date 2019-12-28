13918c361714a11caf29ed6454afda58f5f1bbf8
Szymon Rojek <sz.rojek@gmail.com>
2019-12-28 17:14:46 +0000

    added 1 solution to the 7-kyu/shortest-word.js

--- 7-kyu/shortest-word.js
+++ 7-kyu/shortest-word.js
@@ -3,3 +3,16 @@ Description:
 
 Simple, given a string of words, return the length of the shortest word(s). */
 
+
+
+//  1
+
+const findShort = s => {
+  const wordsArray = s.split(' ');
+  const shortestWord = [];
+  for (let i = 0; i < wordsArray.length; i++) {
+          shortestWord.push(wordsArray[i].length);
+  }
+  const min = Math.min(...shortestWord);
+  return min;
+};
