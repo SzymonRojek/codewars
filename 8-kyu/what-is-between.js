167793d990371543ad7a20a140c5ee2bbe03b0ce
Szymon Rojek <sz.rojek@gmail.com>
2019-12-30 13:33:09 +0000

    added 1 solution to the 8-kyu/what-is-between.js

--- 8-kyu/what-is-between.js
+++ 8-kyu/what-is-between.js
@@ -7,3 +7,13 @@ For example:
 a = 1
 b = 4
 --> [1, 2, 3, 4] */
+
+
+//  1 
+
+const between = (a, b) =>
+        Array(b - a + 1)
+                .fill()
+                .map((_, idx) => a + idx);
+
+console.log(between(-1, 4)); // [-1, 2, 3, 4]
