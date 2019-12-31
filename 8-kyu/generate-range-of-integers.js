1c1d164393cbf2a198ea28cf85d3d25a4623b4fd
Szymon Rojek <sz.rojek@gmail.com>
2019-12-31 13:36:40 +0000

    created 8-kyu/well-of-ideas.js

--- 8-kyu/generate-range-of-integers.js
+++ 8-kyu/generate-range-of-integers.js
@@ -14,3 +14,21 @@ Note
 min < max
 step > 0
 the range does not HAVE to include max (depending on the step) */
+
+
+
+// 1
+
+const generateRange = (min, max, step) => {
+  const result = [];
+  for (let i = min; i <= max; i += step) {
+          result.push(i);
+  }
+  return result;
+};
+
+console.log(generateRange(2, 10, 2)); // [2, 4, 6, 8, 10]
+console.log(generateRange(1, 10, 3)); // [1, 4, 7, 10]
+
+
+
