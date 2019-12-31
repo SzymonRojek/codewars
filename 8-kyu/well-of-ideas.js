81f01e3b751526781a75549b018a4480626b280b
Szymon Rojek <sz.rojek@gmail.com>
2019-12-31 22:22:49 +0000

    added 8 solution to the 8-kyu/well-of-ideas.js

--- 8-kyu/well-of-ideas.js
+++ 8-kyu/well-of-ideas.js
@@ -147,3 +147,20 @@ const well = x => {
         return counter > 2 ? 'I smell a series!' : counter > 0 ? 'Publish!' : 'Fail!';
 };
 
+
+
+// 8
+
+const well = x => {
+        switch (x.filter(el => el === 'good').length) {
+                case 0:
+                        return 'Fail!';
+
+                case 1:
+                case 2:
+                        return 'Publish!';
+
+                default:
+                        return 'I smell a series!';
+        }
+};
