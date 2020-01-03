cd4d9e3206eddd49f7487a18ec71611b5a9dad8c
Szymon Rojek <sz.rojek@gmail.com>
2020-01-03 22:03:42 +0000

    added 1 solution to the 7-kyu/find-duplicates.js

--- 7-kyu/find-duplicates.js
+++ 7-kyu/find-duplicates.js
@@ -5,3 +5,16 @@ Given an array, find the duplicates in that array, and return a new array of tho
 Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., '1' !== 1).
 */
 
+
+
+//  1
+
+const duplicates = arr => {
+  const duplicates = arr.reduce(function(acc, el, i, arr) {
+          if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el);
+          return acc;
+  }, []);
+  return duplicates;
+};
+
+console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, '5'])); // [1, 3, 4]
