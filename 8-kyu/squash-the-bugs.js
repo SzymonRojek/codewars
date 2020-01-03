6aab9ec4c5ebe7c14a7ac84da6f5cab8bc550b84
Szymon Rojek <sz.rojek@gmail.com>
2020-01-03 22:39:59 +0000

    added 1 solution to the 8-kyu/squash-the-bugs.js

--- 8-kyu/squash-the-bugs.js
+++ 8-kyu/squash-the-bugs.js
@@ -3,3 +3,26 @@ so that the code runs and outputs the expected value. Output should
 be the length of the longest word, as a number.
 
 There will only be one 'longest' word.  */
+
+
+
+//  1
+
+const findLongest = str => {
+  const wordsArray = str.split(' ');
+  const longestWord = [];
+
+  for (let i = 0; i < wordsArray.length; i++) {
+          longestWord.push(wordsArray[i].length);
+  }
+
+  const max = Math.max.apply(Math, longestWord); // Math.max(...longestWord)
+  return max;
+};
+
+
+console.log(findLongest('The quick white fox jumped around the massive dog')); // 7
+console.log(findLongest('Take me to tinseltown with you')); // 10
+console.log(findLongest('Sausage chops')); // 7
+console.log(findLongest('Wind your body and wiggle your belly')); // 6
+console.log(findLongest('Lets all go on holiday')); // 7
