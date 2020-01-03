ea106d1953544481348cbf0c7f26a79188cac4ef
Szymon Rojek <sz.rojek@gmail.com>
2020-01-03 23:29:44 +0000

    added 3 solution to the 8-kyu/squash-the-bugs.js

--- 8-kyu/squash-the-bugs.js
+++ 8-kyu/squash-the-bugs.js
@@ -33,3 +33,19 @@ console.log(findLongest('Lets all go on holiday')); // 7
 
 const findLongest = s => Math.max(...s.split(' ').map(x => x.length));
 
+
+
+//  3
+
+const findLongest = str => {
+  const spl = str.split(' ');
+  let longest = 0;
+
+  for (let i = 0; i < spl.length; i++) {
+          if (spl[i].length > longest) {
+                  longest = spl[i].length;
+          }
+  }
+  return longest;
+};
+
