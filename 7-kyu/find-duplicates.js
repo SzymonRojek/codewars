bbbe4ab5f92c75b658b8a54c2072aef23f78d842
Szymon Rojek <sz.rojek@gmail.com>
2020-01-03 22:00:34 +0000

    created 	7-kyu/find-duplicates.js

---
+++ 7-kyu/find-duplicates.js
@@ -0,0 +1,7 @@
+/*
+Description:
+Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array should appear in the order when they first appeared as duplicates.
+
+Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., '1' !== 1).
+*/
+
