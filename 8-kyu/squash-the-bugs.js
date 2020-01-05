9a4c0a00a2f1c328c8514cb308a4501b0afce64c
Szymon Rojek <sz.rojek@gmail.com>
2020-01-05 22:45:03 +0000

    edited 8-kyu/squash-the-bugs.js

--- 8-kyu/squash-the-bugs.js
+++ 8-kyu/squash-the-bugs.js
@@ -1,3 +1,5 @@
+
+
 /*  Simple challenge - eliminate all bugs from the supplied code 
 so that the code runs and outputs the expected value. Output should
 be the length of the longest word, as a number.
