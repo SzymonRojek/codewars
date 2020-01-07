c7a7c980382bb93025502fbd20ae77fd61a33256
Szymon Rojek <sz.rojek@gmail.com>
2020-01-07 00:45:43 +0000

    added 1 solution to the 7-kyu/return-string-of-first-character.js

--- 7-kyu/return-string-of-first-character.js
+++ 7-kyu/return-string-of-first-character.js
@@ -6,3 +6,18 @@ has to be returned with the first character of each word in the string.
 For example:
 
 "This Is A Test" ==> "TIAT"  */
+
+
+
+//  1
+
+const makeString = s =>
+        s
+                .split(' ')
+                .map(el => el[0])
+                .join('');
+
+console.log(makeString('sees eyes xray yoat')); // 'sexy'
+console.log(makeString('brown eyes are nice')); // 'bean'
+console.log(makeString('cars are very nice')); // 'cavn'
+console.log(makeString('kaks de gan has a big head')); // 'kdghabh'
