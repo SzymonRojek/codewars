03c89e705b4b2626dd4778fa2ec14ef974903f68
Szymon Rojek <sz.rojek@gmail.com>
2020-01-07 00:25:18 +0000

    added 1 solution to the 7-kyu/string-return-in-alpabetical-order.js

--- 7-kyu/string-return-in-alpabetical-order.js
+++ 7-kyu/string-return-in-alpabetical-order.js
@@ -2,3 +2,18 @@
 
 /*  The function returns a passed string in an alphabetical 
 order e.g ="fundamentals" answer="aadeflmnnstu" */
+
+
+
+//  1 
+
+const alphabetOrder = str =>
+        str
+                .split('')
+                .sort()
+                .join('');
+
+console.log(alphabetOrder('take')); // 'aekt'
+console.log(alphabetOrder('blaze')); // 'abelz'
+console.log(alphabetOrder('create')); // 'aceert'
+console.log(alphabetOrder('tests')); // 'esstt'
