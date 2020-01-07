dc8d1129b52068737324870fbcb3acf7273caf8f
Szymon Rojek <sz.rojek@gmail.com>
2020-01-07 00:54:09 +0000

    added 2 solution to the 7-kyu/return-string-of-first-character.js

--- 7-kyu/return-string-of-first-character.js
+++ 7-kyu/return-string-of-first-character.js
@@ -21,3 +21,17 @@ console.log(makeString('sees eyes xray yoat')); // 'sexy'
 console.log(makeString('brown eyes are nice')); // 'bean'
 console.log(makeString('cars are very nice')); // 'cavn'
 console.log(makeString('kaks de gan has a big head')); // 'kdghabh'
+
+
+
+//  2
+
+const makeString = s => {
+  const newArr = [];
+  s.split(' ').forEach(element => {
+          if (element[0] !== undefined) {
+                  newArr.push(element[0]);
+          }
+  });
+  return newArr.join('');
+};
