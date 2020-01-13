6a1d682610eb4f8bdd13b71b24d4bfcfd7aa134f
Szymon Rojek <sz.rojek@gmail.com>
2020-01-13 23:29:17 +0000

    added 1 solution to the  8-kyu/convert-number-to-array-of-digits.js

--- 8-kyu/convert-number-to-array-of-digits.js
+++ 8-kyu/convert-number-to-array-of-digits.js
@@ -7,3 +7,17 @@ You have to return the digits of this number within an array in reverse order.
 
 Example:
 348597 => [7,9,5,8,4,3]   */
+
+
+//  1
+
+const digitize = n => {
+  
+  n = `${n}`.split('');
+
+  return n.map(Number).reverse();
+};
+
+console.log(digitize(35231)); // [1, 3, 2, 5, 3]
+
+
