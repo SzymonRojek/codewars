314ce8f1ecf814b10e568b4584618d6a022c0c09
Szymon Rojek <sz.rojek@gmail.com>
2020-01-14 15:08:19 +0000

    added 2 solution to the 8-kyu/grasshooper-terminalGameMoveFunction.js

--- 8-kyu/grasshooper-terminalGameMoveFunction.js
+++ 8-kyu/grasshooper-terminalGameMoveFunction.js
@@ -23,3 +23,11 @@ console.log(move(0, 4)); // 8
 console.log(move(3, 6)); // 15
 console.log(move(2, 5)); // 12
 
+
+//  2
+
+const move = (position, roll) => position + roll + roll;
+
+console.log(move(0, 4)); // 8
+console.log(move(3, 6)); // 15
+console.log(move(2, 5)); // 12
