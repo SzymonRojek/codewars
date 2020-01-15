251ce8538e5a062a2ff4013c057c4939028444ff
Szymon Rojek <sz.rojek@gmail.com>
2020-01-15 23:24:25 +0000

    added 1 solution to the 8-kyu/convert-a-string-to-an-array.js

--- 8-kyu/convert-a-string-to-an-array.js
+++ 8-kyu/convert-a-string-to-an-array.js
@@ -7,3 +7,13 @@ Write a function to split a string and convert it into an array of words. For ex
 "Robin Singh" ==> ["Robin", "Singh"]
 
 "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]   */
+
+
+
+//  1
+
+const stringToArray = string => string.split(' ');
+
+console.log(stringToArray('Robin Singh')); // ['Robin', 'Singh']
+console.log(stringToArray('I love arrays they are my favorite'));
+// ['I', 'love', 'arrays', 'they', 'are', 'my', 'favorite']
