318b5f178c4f646e9979315fff3bd8f74778372e
Szymon Rojek <sz.rojek@gmail.com>
2020-01-16 23:44:46 +0000

    added 3 solution to the 8-kyu/grasshooper-personalizedMessage.js

--- 8-kyu/grasshooper-personalizedMessage.js
+++ 8-kyu/grasshooper-personalizedMessage.js
@@ -35,3 +35,12 @@ const greet = (name, owner) => (name === owner ? 'Hello boss' : 'Hello quest');
 
 console.log(greet('Daniel', 'Daniel')); // 'Hello boss'
 console.log(greet('Greg', 'Daniel')); // 'Hello guest'
+
+
+
+//  3
+
+const greet = (name, owner) => `Hello ${name == owner ? 'boss' : 'guest'}`;
+
+console.log(greet('Daniel', 'Daniel')); // 'Hello boss'
+console.log(greet('Greg', 'Daniel')); // 'Hello guest'
