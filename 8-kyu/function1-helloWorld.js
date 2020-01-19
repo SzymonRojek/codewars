709b5fe7cf6f418a74cbafeeb0753183bb8452c3
Szymon Rojek <sz.rojek@gmail.com>
2020-01-19 22:15:44 +0000

    added 1 solution to the 8-kyu/function1-helloWorld.js

--- 8-kyu/function1-helloWorld.js
+++ 8-kyu/function1-helloWorld.js
@@ -7,3 +7,15 @@ Style Points
 Sure, this is about as easy as it gets. 
 But how clever can you be to create the most creative hello world you can think of? 
 What is a "hello world" solution you would want to show your friends?   */
+
+
+
+//  1
+
+const greet = x => 'hello world!';
+
+console.log(greet());
+
+
+
+
