46f9faf4d07dd26104b0418f6d8af6a098040e00
Szymon Rojek <sz.rojek@gmail.com>
2020-01-21 21:39:59 +0000

    added 1 solution to the 7-kyu/flatt-sort-array.js

--- 7-kyu/flatt-sort-array.js
+++ 7-kyu/flatt-sort-array.js
@@ -17,3 +17,14 @@ Please, keep in mind, that JavaScript is by default sorting objects alphabetical
 For more information, please consult:
 
 http://stackoverflow.com/questions/6093874/why-doesnt-the-sort-function-of-javascript-work-well   */
+
+
+
+//  1
+
+const flattenAndSort = array => Array.prototype.concat.apply([], array).sort((a, b) => a - b);
+
+console.log(flattenAndSort([])); // []
+console.log(flattenAndSort([[], [1]])); // [1]
+console.log(flattenAndSort([[3, 2, 1],[7, 9, 8],[6, 4, 5],])); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]])); //  [1, 2, 3, 4, 5, 6, 100]
