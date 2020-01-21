2e84191b201644abd0576ce86a04d5f738cc98f8
Szymon Rojek <sz.rojek@gmail.com>
2020-01-21 21:43:21 +0000

    added 4 solution to the 7-kyu/flatt-sort-array.js

--- 7-kyu/flatt-sort-array.js
+++ 7-kyu/flatt-sort-array.js
@@ -40,3 +40,18 @@ const flattenAndSort = array => [].concat(...array).sort((a, b) => a - b);
 //  3
 
 const flattenAndSort = array => array.reduce((result, current) => [...result, ...current], []).sort((a, b) => a - b);
+
+
+
+//  4
+
+function flattenAndSort(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+          for (let j = 0; j < array[i].length; j++) {
+                  newArray.push(array[i][j]);
+          }
+  }
+
+  return newArray.sort((a, b) => a - b);
+}
