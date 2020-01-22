30488a15a47cad1ad47ee51cff0aacd3f872de0d
Szymon Rojek <sz.rojek@gmail.com>
2020-01-22 12:31:16 +0000

    added 2 solution to the 8-kyu/array-plus-array.js

--- 8-kyu/array-plus-array.js
+++ 8-kyu/array-plus-array.js
@@ -16,3 +16,9 @@ console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); // 21
 console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6])); // -21
 console.log(arrayPlusArray([0, 0, 0], [4, 5, 6])); // 15
 console.log(arrayPlusArray([100, 200, 300], [400, 500, 600])); // 2100
+
+
+
+//  2
+
+const arrayPlusArray = (...arrays) => [].concat(...arrays).reduce((a, b) => a + b, 0);
