fdb9170811b204be0389e38851415508e634e574
Szymon Rojek <sz.rojek@gmail.com>
2020-01-22 13:04:20 +0000

    added 8 solution to the 8-kyu/array-plus-array.js

--- 8-kyu/array-plus-array.js
+++ 8-kyu/array-plus-array.js
@@ -81,3 +81,15 @@ function arrayPlusArray() {
   }
   return res;
 }
+
+
+
+//  8
+
+function arrayPlusArray(arr1, arr2) {
+  let sum = 0;
+  for (i = 0; i < arr1.length; i++) {
+          sum += arr1[i] + arr2[i];
+  }
+  return sum;
+}
