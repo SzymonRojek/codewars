7be82f1c074115d6351b82934de2ecbcdfd81547
Szymon Rojek <sz.rojek@gmail.com>
2020-01-23 22:41:14 +0000

    added 1 solution to the  8-kyu/reversed-sequence.js

--- 8-kyu/reversed-sequence.js
+++ 8-kyu/reversed-sequence.js
@@ -5,3 +5,14 @@ Get the number n (n>0) to return the reversed sequence from n to 1.
 
 Example : n=5 >> [5,4,3,2,1]  */
 
+
+
+//  1
+
+const reverseSeq = n =>
+        Array(n)
+                .fill(1)
+                .map((v, i) => i + 1)
+                .reverse();
+
+console.log(reverseSeq(5)); // [5, 4, 3, 2, 1]
