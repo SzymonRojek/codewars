890f0850a8b150ce1352fca6fa25c2654326bfbb
Szymon Rojek <sz.rojek@gmail.com>
2020-01-25 23:09:09 +0000

    created file 	8-kyu/find-numbers-divisible-by-given-number.js

--- 8-kyu/find-numbers-divisible-by-given-number.js
+++ 8-kyu/find-numbers-divisible-by-given-number.js
@@ -6,3 +6,25 @@ First argument is an array of numbers and the second is the divisor.
 
 Example
 divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]   */
+
+
+
+//  1
+
+const divisibleBy = (numbers, divisor) => {
+  const array = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+          if (numbers[i] % divisor === 0) {
+                  array.push(numbers[i]);
+          }
+  }
+
+  return array;
+};
+
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2)); // [2, 4, 6]
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3)); //  [3, 6]
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4)); // [0, 4]
+console.log(divisibleBy([0], 4)); // [0]
+console.log(divisibleBy([1, 3, 5], 2)); // []
