d0fb5cf2cb1def4ea03f3179388a3e0d9210dee3
Szymon Rojek <sz.rojek@gmail.com>
2020-01-26 22:32:37 +0000

    added 7 solution to the 	8-kyu/square-n-sum.js

--- 8-kyu/square-n-sum.js
+++ 8-kyu/square-n-sum.js
@@ -105,3 +105,18 @@ const squareSum = numbers => {
 
 console.log(squareSum([1, 2])); // 5
 console.log(squareSum([0, 3, 4, 5])); // 50
+
+
+
+//  7
+
+function squareSum(numbers) {
+  let result = 0;
+  for (let i = 0; i < numbers.length; i++) {
+          result += numbers[i] * numbers[i];
+  }
+  return result;
+}
+
+console.log(squareSum([1, 2])); // 5
+console.log(squareSum([0, 3, 4, 5])); // 50
