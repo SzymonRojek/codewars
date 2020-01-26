eb1c9a82149a14ac4c99734c19ded32db0a39cfc
Szymon Rojek <sz.rojek@gmail.com>
2020-01-26 22:12:58 +0000

    added 1 solution to the 	8-kyu/square-n-sum.js

--- 8-kyu/square-n-sum.js
+++ 8-kyu/square-n-sum.js
@@ -8,3 +8,23 @@ passed into it and then sums the results together.
 For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 */
+
+
+
+//  1
+
+const squareSum = numbers => {
+  const square = [];
+  for (let i = 0; i < numbers.length; i++) {
+          square.push(numbers[i] ** 2);
+  }
+
+  let sum = 0;
+  for (let j = 0; j < square.length; j++) {
+          sum += square[j];
+  }
+  return sum;
+};
+
+console.log(squareSum([1, 2])); // 5
+console.log(squareSum([0, 3, 4, 5])); // 50
