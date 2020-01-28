329eddadedf820ed01eb95d9479cf613ce99b6de
Szymon Rojek <sz.rojek@gmail.com>
2020-01-28 22:14:56 +0000

    added 2 solution to the 8-kyu/what-is-the-real-floor.js

--- 8-kyu/what-is-the-real-floor.js
+++ 8-kyu/what-is-the-real-floor.js
@@ -36,3 +36,13 @@ const getRealFloor = n => {
 console.log(getRealFloor(1)); // 0
 console.log(getRealFloor(5)); // 4
 console.log(getRealFloor(15)); // 13
+
+
+
+//  2
+
+const getRealFloor = n => (n > 13 ? n - 2 : n > 0 ? n - 1 : n);
+
+console.log(getRealFloor(1)); // 0
+console.log(getRealFloor(5)); // 4
+console.log(getRealFloor(15)); // 13
