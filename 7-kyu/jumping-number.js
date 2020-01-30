8286f75ad0d4e6d4f0077aae039fba75c14cb59a
Szymon Rojek <sz.rojek@gmail.com>
2020-01-30 22:15:00 +0000

    created file 	7-kyu/jumping-number.js

---
+++ 7-kyu/jumping-number.js
@@ -0,0 +1,9 @@
+
+
+/*  Number passed is always Positive .
+
+Return the result as String .
+
+The difference between ‘9’ and ‘0’ is not considered as 1 .
+
+All single digit numbers are considered as Jumping numbers. */
