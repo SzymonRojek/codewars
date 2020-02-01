baf5205241ab92b17434baa397e09ee9d753bc6f
Szymon Rojek <sz.rojek@gmail.com>
2020-02-01 22:23:25 +0000

    created file 	7-kyu/very-even-number.js

---
+++ 7-kyu/very-even-number.js
@@ -0,0 +1,21 @@
+
+
+/*
+Description:
+
+Write a function that returns true if the number is a "Very Even" number.
+
+If the number is odd, it is not a "Very Even" number.
+
+If the number is even, return whether the sum of the digits is a "Very Even" number.
+
+Examples:
+
+input(88) => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+
+input(222) => returns true
+
+input(5) => returns false
+
+Note: The numbers will always be positive!
+*/
