a67f09571659da2dbcd02072c81ea92f62e00fb1
Szymon Rojek <sz.rojek@gmail.com>
2020-02-03 23:43:39 +0000

    added 2 solution to the 	7-kyu/remove-duplicate-words.js

--- 7-kyu/remove-duplicate-words.js
+++ 7-kyu/remove-duplicate-words.js
@@ -31,3 +31,11 @@ function removeDuplicateWords(s) {
 }
 
 console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
+
+
+
+//  2
+
+const removeDuplicateWords = s => [...new Set(s.split(' '))].join(' ');
+
+console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
