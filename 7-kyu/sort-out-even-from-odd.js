694b29d6cfc0811a980475e52e2597838b88cd9e
Szymon Rojek <sz.rojek@gmail.com>
2020-02-06 21:57:18 +0000

    created file 7-kyu/sort-out-even-from-odd.js

---
+++ 7-kyu/sort-out-even-from-odd.js
@@ -0,0 +1,14 @@
+
+
+/*
+Return an array/list where Even numbers come first then odds
+
+Since , Men are stronger than Boys , Then Even numbers in ascending order While odds in descending .
+
+Array/list size is at least *4*** .
+
+Array/list numbers could be a mixture of positives , negatives .
+
+Have no fear , It is guaranteed that no Zeroes will exists . !alt
+
+Repetition of numbers in the array/list could occur , So (duplications are not included when separating).   */
