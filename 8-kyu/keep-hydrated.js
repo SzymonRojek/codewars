3a8f8563fd94afb65fbb4bb6f7788f70953baa7b
Szymon Rojek <sz.rojek@gmail.com>
2020-02-07 23:32:55 +0000

    added 1 solution to the  8-kyu/keep-hydrated.js

--- 8-kyu/keep-hydrated.js
+++ 8-kyu/keep-hydrated.js
@@ -17,3 +17,25 @@ time = 6.7---> litres = 3
 time = 11.8--> litres = 5
 
 */
+
+
+
+//  1
+
+function litres(time) {
+  const getMinutes = time * 60;
+
+  const drinkPerHour = 0.5;
+
+  const toDrink = parseInt((getMinutes * drinkPerHour) / 60);
+
+  return toDrink;
+}
+
+console.log(litres(2)); // 1, 'should return 1 litre'
+console.log(litres(1.4)); // 0, 'should return 0 litres'
+console.log(litres(12.3)); // 6, 'should return 6 litres'
+console.log(litres(0.82)); // 0, 'should return 0 litres'
+console.log(litres(11.8)); // 5, 'should return 5 litres'
+console.log(litres(1787)); // 893, 'should return 893 litres'
+console.log(litres(0)); // 0, 'should return 0 litres'
