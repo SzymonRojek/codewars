662487a568431b77bcbb503354f3267852b3f46e
Szymon Rojek <sz.rojek@gmail.com>
2020-02-07 16:32:42 +0000

    added 1 solution to the  7-kyu/sum-of-minimums-arrays.js

--- 7-kyu/sum-of-minimums-arrays.js
+++ 7-kyu/sum-of-minimums-arrays.js
@@ -15,3 +15,33 @@ So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
 
 Note: You will be always given non-empty array containing Positive values.
 */
+
+
+
+//  1
+
+const sumOfMinimums = arr => {
+  const minValues = [];
+  for (let i = 0; i < arr.length; i++) {
+          minValues.push(Math.min(...arr[i]));
+  }
+  const addMinValues = (a, b) => a + b;
+
+  return minValues.reduce(addMinValues);
+};
+
+const sum = console.log(
+  sumOfMinimums([
+          [7, 9, 8, 6, 2],
+          [6, 3, 5, 4, 3],
+          [5, 8, 7, 4, 5],
+  ])
+); // 9);
+console.log(
+  sumOfMinimums([
+          [11, 12, 14, 54],
+          [67, 89, 90, 56],
+          [7, 9, 4, 3],
+          [9, 8, 6, 7],
+  ])
+); // 76
