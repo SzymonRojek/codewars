f0acbad6921db4f23ef052353beb0c789a2bc48b
Szymon Rojek <sz.rojek@gmail.com>
2020-02-08 18:13:37 +0000

    added 1 solution to the 7-kyu/no-oddities.js

--- 7-kyu/no-oddities.js
+++ 7-kyu/no-oddities.js
@@ -8,3 +8,22 @@ All values in the array will be integers. Return the good values in the order th
 
 function noOdds( values )
 */
+
+
+
+//  1
+
+const noOdds = values => {
+
+  const even = [];
+  
+  for (let i = 0; i < values.length; i++) {
+          if (values[i] % 2 === 0) {
+                  even.push(values[i]);
+          }
+  }
+  return even;
+};
+
+console.log(noOdds([0, 1])); // [0]
+console.log(noOdds([0, 1, 2, 3])); // [0,2]
