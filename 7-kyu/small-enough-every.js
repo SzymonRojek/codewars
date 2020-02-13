26805fd4eaf16ba1781437b4aa7179f8cb7f7cab
Szymon Rojek <sz.rojek@gmail.com>
2020-02-13 10:50:16 +0000

    added 2 solution to the 7-kyu/small-enough-every.js

--- 7-kyu/small-enough-every.js
+++ 7-kyu/small-enough-every.js
@@ -24,3 +24,9 @@ console.log(smallEnough([66, 101], 200)); // true
 console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); // false
 console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); // true
 console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); // true
+
+
+
+//  2
+
+const smallEnough = (a, limit) => Math.max(...a) <= limit;
