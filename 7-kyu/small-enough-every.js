9abc8b216624560118fd9b611240bd76792da6d5
Szymon Rojek <sz.rojek@gmail.com>
2020-02-13 10:49:24 +0000

    added 1 solution to the 7-kyu/small-enough-every.js

--- 7-kyu/small-enough-every.js
+++ 7-kyu/small-enough-every.js
@@ -1,3 +1,5 @@
+
+
 /*
 Description:
 * No Loops Allowed *
@@ -12,3 +14,13 @@ Do not use loops. Do not modify input array.
 
 Looking for more, loop-restrained fun? Check out the other kata in the series:
 */
+
+
+// 1
+
+const smallEnough = (a, limit) => a.every(e => e <= limit);
+
+console.log(smallEnough([66, 101], 200)); // true
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); // false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); // true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); // true
