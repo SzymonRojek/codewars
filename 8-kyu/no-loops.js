bcb9de303118b546f6e96fe1d568e22dc02a46a6
Szymon Rojek <sz.rojek@gmail.com>
2020-02-23 22:15:06 +0000

    added 2 solution to the 8-kyu/no-loops.js

--- 8-kyu/no-loops.js
+++ 8-kyu/no-loops.js
@@ -25,3 +25,15 @@ const check = (a, x) => {
 
   return checkTheValue;
 };
+
+console.log(check([66, 101], 66)); // true
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45)); // true
+console.log(check(['t', 'e', 's', 't'], 'e')); // true
+console.log(check(['what', 'a', 'great', 'kata'], 'kat')); // false
+
+
+
+//  2
+
+check = (a, x) => a.some(v => v === x);
+
