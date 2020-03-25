7dea7b2197c9597a9ffa45005fd3555200774388
Szymon Rojek <sz.rojek@gmail.com>
2020-03-25 20:10:30 +0100

    added 3 solution to the 7-kyu/extra-perfect-getOdd.js

--- 7-kyu/extra-perfect-getOdd.js
+++ 7-kyu/extra-perfect-getOdd.js
@@ -32,6 +32,12 @@ console.log(extraPerfect(28)); // [1,3,5,7,9,11,13,15,17,19,21,23,25,27]
 console.log(extraPerfect(39)); // [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]
 
 
+
 //  2
 
 const extraPerfect = n => [...Array(n + 1).keys()].filter(cur => cur % 2);
+
+
+//  3
+
+const extraPerfect = n => new Array(Math.ceil(n / 2)).fill(0).map((a, i) => i * 2 + 1);
