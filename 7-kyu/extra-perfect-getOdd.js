69bd965b825e074351a4c1475a219fea3283ce96
Szymon Rojek <sz.rojek@gmail.com>
2020-03-25 20:02:59 +0100

    created a file in the 	7-kyu/extra-perfect-getOdd.js

---
+++ 7-kyu/extra-perfect-getOdd.js
@@ -0,0 +1,12 @@
+
+
+/*
+Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
+
+Number passed is always Positive .
+
+Returned array/list should contain the extra perfect numbers in ascending order from lowest to highest
+
+extraPerfect(3)  ==>  return {1,3}
+
+*/
