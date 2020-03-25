0eca243c784cc58d277fab18957057e7588a1583
Szymon Rojek <sz.rojek@gmail.com>
2020-03-25 20:04:07 +0100

    added 1 solution to the 	7-kyu/extra-perfect-getOdd.js

--- 7-kyu/extra-perfect-getOdd.js
+++ 7-kyu/extra-perfect-getOdd.js
@@ -10,3 +10,24 @@ Returned array/list should contain the extra perfect numbers in ascending order 
 extraPerfect(3)  ==>  return {1,3}
 
 */
+
+
+//  1
+
+function extraPerfect(n) {
+  const odd = [];
+
+  for (let i = 0; i <= n; i++) {
+          if (i % 2 !== 0) {
+                  odd.push(i);
+          }
+  }
+  return odd;
+}
+
+console.log(extraPerfect(3)); // [1,3]
+console.log(extraPerfect(5)); // [1,3,5]
+console.log(extraPerfect(7)); //  [1,3,5,7]
+console.log(extraPerfect(28)); // [1,3,5,7,9,11,13,15,17,19,21,23,25,27]
+console.log(extraPerfect(39)); // [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]
+
