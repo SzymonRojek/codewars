43dd2f84140052c8445c803fb3e3c37e5f563645
Szymon Rojek <sz.rojek@gmail.com>
2020-03-29 19:34:53 +0200

    added 3 solution to the file 8-kyu/all-star-code-challenge#18.js

--- 8-kyu/all-star-code-challenge#18.js
+++ 8-kyu/all-star-code-challenge#18.js
@@ -35,3 +35,18 @@ console.log(strCount('Hello', 'l'));
 function strCount(str, letter) {
   return str.split('').filter(c => c == letter).length;
 }
+
+
+
+//  3
+
+function strCount(str, letter) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+          if (str[i] === letter) {
+                  count++;
+          }
+  }
+  return count;
+}
