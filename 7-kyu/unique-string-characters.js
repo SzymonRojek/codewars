1670c2b43d01c99cc5bfa1b9a80a1e02e86e7c21
Szymon Rojek <sz.rojek@gmail.com>
2020-05-20 13:19:00 +0200

    created new file in the 7-kyu/unique-string-characters.js

---
+++ 7-kyu/unique-string-characters.js
@@ -0,0 +1,17 @@
+
+
+
+/*
+
+Description:
+In this Kata, you will be given two strings a and b and your task will be to return the characters 
+that are not common in the two strings.
+
+For example:
+
+solve("xyab","xzca") = "ybzc" 
+--The first string has 'yb' which is not in the second string. 
+--The second string has 'zc' which is not in the first string. 
+
+*/
+
