db982eaa589e3ac2ff9a1190edfa272df589e313
Szymon Rojek <sz.rojek@gmail.com>
2020-05-20 13:28:57 +0200

    edited 4 solution in the file 7-kyu/unique-string-characters.js

--- 7-kyu/unique-string-characters.js
+++ 7-kyu/unique-string-characters.js
@@ -65,19 +65,17 @@ const solve = (a, b) => [...a, ...b].reduce((acc, cur) => (
 
 // 4
 
-function solve(a, b){
+const solve = (a, b) => {
   let result = '';
-  a.split('').forEach(charFromA => {
-      if (!b.includes(charFromA)) {
-          result = result + charFromA;
-      }
-  });
-
-  b.split('').forEach(charFromB => {
-      if (!a.includes(charFromB)) {
-          result = result + charFromB;
-      }
-  });
-  
- return result;
+  for (let i = 0; i < a.length; i++) {
+    if (!(b.includes(a[i]))) {
+      result += a[i];
+    }
+  }
+  for (let i = 0; i < b.length; i++) {
+    if (!(a.includes(b[i]))) {
+      result += b[i];
+    }
+  }
+  return result;
 };
