46153e729fb4c6f44e22005ff270c56ee803ca1e
Szymon Rojek <sz.rojek@gmail.com>
2020-05-23 23:53:27 +0200

    added 2 solution to the file in 8-kyu/get-planet-name.js

--- 8-kyu/get-planet-name.js
+++ 8-kyu/get-planet-name.js
@@ -40,3 +40,22 @@ function getPlanetName(id){
   }
   return name;
 }
+
+
+
+// 2:
+
+function getPlanetName(id){
+  var name = {
+    1: 'Mercury',
+    2: 'Venus',
+    3: 'Earth',
+    4: 'Mars',
+    5: 'Jupiter',
+    6: 'Saturn',
+    7: 'Uranus',
+    8: 'Neptune',
+  };
+  
+  return name[id];
+}
