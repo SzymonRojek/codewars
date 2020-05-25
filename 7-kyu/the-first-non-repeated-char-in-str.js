d06598e5d9693a4f4428199cb98d4b2b5fab5e04
Szymon Rojek <sz.rojek@gmail.com>
2020-05-25 23:45:25 +0200

    added 1 solution to the file in  7-kyu/the-first-non-repeated-char-in-str.js

--- 7-kyu/the-first-non-repeated-char-in-str.js
+++ 7-kyu/the-first-non-repeated-char-in-str.js
@@ -15,3 +15,27 @@ You can assume, that the input string has always non-zero length.
 
 If there is no repeating character, return null in JS or Java, and None in Python.
 */
+
+
+
+// 1:
+
+  
+function firstNonRepeated(str) {
+ 
+  const duplicates = [...str].reduce((acc, el, i, arr) => {
+    if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el); 
+
+    return acc;
+  }, []);
+
+  const unique = [...str].filter(el => duplicates.indexOf(el) === -1).join('')[0];
+
+   return unique || null;
+ }
+
+console.log(firstNonRepeated("test")); // e
+console.log(firstNonRepeated("teeter")); // r
+console.log(firstNonRepeated("1122321235121222")); // 5
+console.log(firstNonRepeated('arek')); // a  
+console.log(firstNonRepeated('aabb')); // null
