28437960a1ae53b17e0f04f373bb7463d210c06d
Szymon Rojek <sz.rojek@gmail.com>
2020-05-29 16:18:38 +0200

    added 2 solution to the file in 	7-kyu/sum-even-numbers.js

--- 7-kyu/sum-even-numbers.js
+++ 7-kyu/sum-even-numbers.js
@@ -32,3 +32,9 @@ function sumEvenNumbers(arr) {
  
 console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 30
 console.log(sumEvenNumbers([2, 4, 3, 10, 2, 1, 0, 1, 2])); // 20
+
+
+
+// 2:
+
+const sumEvenNumbers = input => input.filter(x => x % 2 == 0).reduce((x, y) => x + y, 0);
