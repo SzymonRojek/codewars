ccc52a0d0bc5adb1f071805324fe7b1ce5ce8d59
Szymon Rojek <sz.rojek@gmail.com>
2020-06-09 20:39:22 +0100

    added 2 solution to the file 7-kyu/roundUp-to-next-multiplieOf-5.js

--- 7-kyu/roundUp-to-next-multiplieOf-5.js
+++ 7-kyu/roundUp-to-next-multiplieOf-5.js
@@ -22,3 +22,41 @@ Input may be any positive or negative integer (including 0).
 You can assume that all inputs are valid integers.
 */ 
 
+
+
+// 1:
+
+function roundToNext5(n){
+  let result = [];
+ 
+  // positives num
+    for (let i = 0; i < n;  i += 5) {
+      result.push(i+5);
+    }
+ 
+  // negatives num
+    for (let i = 0; i >= n; i -= 5) {
+      result.push(i);  
+    }
+ 
+ return (n === 0) ? 0 : result[result.length-1];
+}
+
+console.log(roundToNext5(0)); // 0
+console.log(roundToNext5(4)); // 4
+console.log(roundToNext5(6)); // 10
+console.log(roundToNext5(30)); // 30
+console.log(roundToNext5(35)); // 35
+console.log(roundToNext5(441)); // 445
+console.log(roundToNext5(-1)); // 0
+console.log(roundToNext5(-5)); // -5
+console.log(roundToNext5(-7)); // -5
+console.log(roundToNext5(-12)); // -10
+
+
+
+// 2:
+
+function roundToNext5(n){
+  return Math.ceil(n/5)*5;
+}
