2d2803016b99802ece615d6d3589e182aabf1ab1
Szymon Rojek <sz.rojek@gmail.com>
2020-06-12 20:17:21 +0100

    added 1 solution to the file in 8-kyu/find-something-in-array.js

--- 8-kyu/find-something-in-array.js
+++ 8-kyu/find-something-in-array.js
@@ -11,3 +11,11 @@ For example:
 find("hello", ["bye bye","hello"]) // return true
 find("anything", ["bye bye","hello"]) // return false
 */
+
+
+// 1 my solution:
+
+const find = (string, array) => array.includes(string);
+
+console.log(find("hello", ["bye bye","hello"])); // true
+console.log(find("anything", ["bye bye","hello"])); // false
