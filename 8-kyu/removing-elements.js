ea5bbb0e6c52cbc1b3ffab3a0067c4d689ff0227
Szymon Rojek <sz.rojek@gmail.com>
2020-06-12 23:46:34 +0100

    added 4 solution to the file in 8-kyu/removing-elements.js

--- 8-kyu/removing-elements.js
+++ 8-kyu/removing-elements.js
@@ -51,3 +51,16 @@ function removeEveryOther(arr){
   return newArr;
 }
 
+
+
+// 4:
+
+function removeEveryOther(arr){
+  let newArr=[];
+
+    for (let i = 0; i < arr.length; i+=2){
+      newArr.push(arr[i]);
+    }
+
+  return newArr;
+}
