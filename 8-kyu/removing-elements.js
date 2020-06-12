012306717885c8d36beb61aae4dbddb1420e7a2b
Szymon Rojek <sz.rojek@gmail.com>
2020-06-12 23:40:17 +0100

    added 1 solution to the file in 8-kyu/removing-elements.js

--- 8-kyu/removing-elements.js
+++ 8-kyu/removing-elements.js
@@ -11,3 +11,19 @@ Example:
 myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
 None of the arrays will be empty, so you don't have to worry about that!
 */
+
+
+// 1: my solution
+
+function removeEveryOther(arr){
+  let newArr = [];
+
+  arr.forEach((num, i) => { if(i % 2 === 0) newArr.push(num) });
+
+  return newArr;
+}
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])); // ['Hello', 'Hello Again']
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // [1, 3, 5, 7, 9]
+console.log(removeEveryOther([[1, 2]])); // [[1, 2]]
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}])); // [['Goodbye']]
