09b9c33de7c7dcb0d24983443228c8adf8ae9c2b
Szymon Rojek <sz.rojek@gmail.com>
2020-07-04 23:00:12 +0100

    added 4 solution to the file  8-kyu/how-good-are-you.js

--- 8-kyu/how-good-are-you.js
+++ 8-kyu/how-good-are-you.js
@@ -76,3 +76,15 @@ console.log(betterThanAverage([2, 3], 5)); // true
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)); // true
   
 console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)); // false
+
+
+
+
+// 4:
+
+const sum = array => array.reduce((a, b) => a + b);
+const average = array => sum(array) / array.length;
+
+const betterThanAverage = (classPoints, yourPoints) => yourPoints > average(classPoints);
+
+
