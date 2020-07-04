033c60789c385281a1dbcb35f2bbf6404e6ac304
Szymon Rojek <sz.rojek@gmail.com>
2020-07-04 22:50:54 +0100

    added 1 solution to the file  8-kyu/how-good-are-you.js

--- 8-kyu/how-good-are-you.js
+++ 8-kyu/how-good-are-you.js
@@ -18,3 +18,29 @@ Note:
 Your points are not included in the array of your class's points. 
 For calculating the average point you may add your point to the given array!
 */
+
+
+
+// 1: my solution
+
+function betterThanAverage(classPoints, yourPoints) {
+  
+  let sumArr = 0;
+  const lengthArr = classPoints.length;
+
+  classPoints.forEach(el => {
+    sumArr += el;
+  });
+
+  const average = Math.floor(sumArr / lengthArr);
+
+  return yourPoints > average? true : false;
+
+}
+
+
+console.log(betterThanAverage([2, 3], 5)); // true
+
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)); // true
+  
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)); // false
