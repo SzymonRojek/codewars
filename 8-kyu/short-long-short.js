92811d68d6d41978c027fd748c1529471afb3723
Szymon Rojek <sz.rojek@gmail.com>
2020-07-09 23:28:55 +0100

    added 1 solution to the file 8-kyu/short-long-short.js

--- 8-kyu/short-long-short.js
+++ 8-kyu/short-long-short.js
@@ -12,3 +12,14 @@ For example:
 solution("1", "22") // returns "1221"
 solution("22", "1") // returns "1221"
 */
+
+
+
+// 1: my solution
+
+const solution = (a, b) => a.length > b.length ? b + a + b : a + b + a;
+
+console.log(solution('45', '1')); //  '1451'
+console.log(solution('13', '200')); // '1320013'
+console.log(solution('Soon', 'Me')); // 'MeSoonMe'
+console.log(solution('U', 'False')); // 'UFalseU'
