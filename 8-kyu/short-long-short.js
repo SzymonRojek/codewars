41484867350fb055d2395106f78531edacc83e96
Szymon Rojek <sz.rojek@gmail.com>
2020-07-09 23:32:29 +0100

    added 2 solution to the file 8-kyu/short-long-short.js

--- 8-kyu/short-long-short.js
+++ 8-kyu/short-long-short.js
@@ -23,3 +23,15 @@ console.log(solution('45', '1')); //  '1451'
 console.log(solution('13', '200')); // '1320013'
 console.log(solution('Soon', 'Me')); // 'MeSoonMe'
 console.log(solution('U', 'False')); // 'UFalseU'
+
+
+
+// 2:
+
+
+function solution(a, b) {
+
+  if (a.length < b.length) return a + b + a;
+  
+  return b + a + b;
+}
