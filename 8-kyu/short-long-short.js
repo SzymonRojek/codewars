3c39913e8268fed1ac483d5d319f4f84a277d707
Szymon Rojek <sz.rojek@gmail.com>
2020-07-10 23:56:40 +0100

    edited file 8-kyu/short-long-short.js

--- 8-kyu/short-long-short.js
+++ 8-kyu/short-long-short.js
@@ -3,6 +3,7 @@
 
 /*
 Description:
+
 Given 2 strings, a and b, return a string of the form short+long+short, 
 with the shorter string on the outside and the longer string on the inside. 
 The strings will not be the same length, but they may be empty ( length 0 ).
@@ -35,7 +36,6 @@ function solution(a, b) {
 
   return b + a + b;
 }
-
 
 
 // 3:
