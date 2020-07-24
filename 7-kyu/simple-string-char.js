32bc8e511a4d6437f43acc6ac078f39c6f3723cc
Szymon Rojek <sz.rojek@gmail.com>
2020-07-24 23:20:29 +0100

    added 2 solution to the file 7-kyu/simple-string-char.js

--- 7-kyu/simple-string-char.js
+++ 7-kyu/simple-string-char.js
@@ -47,3 +47,44 @@ console.log(solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H")); // [9,9,6,9]
 console.log(solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD")); // [15,8,6,9]
 console.log(solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe")); // [10,7,3,6]
 console.log(solve("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft")); // [7,13,4,10]
+
+
+
+
+// 2: my solution
+
+function solve(s){
+  
+  s = s.split('');
+
+  let finalArr = [];
+  let checkUpper = 0;
+  let checkLower = 0;
+  let checkNumber = 0;
+  let checkCharacter = 0;
+
+  s.forEach(el => {
+    if(/^[A-Z]*$/.test(el)) checkUpper++;
+    if(/^[a-z]*$/.test(el)) checkLower++;
+    if(/^[0-9]*$/.test(el)) checkNumber++;
+  });
+
+  
+  let specChar = "^[|~`\"\'{}:;<>!€‹™@#$%\/&*?,()+=._-]";
+  specChar = specChar.split('');
+
+  for (let i = 0; i < s.length; i++) {
+    const el = s[i];
+
+    for (let j = 0; j < specChar.length; j++) {
+      const char = specChar[j];
+      
+      if(el === char) checkCharacter++;
+    }
+  }
+ 
+  finalArr.push(checkUpper, checkLower, checkNumber,checkCharacter);
+
+  return finalArr;
+ }
+
