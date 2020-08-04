43c5a8db019fafc00ea05b48fd7a549aac4d6a3c
Szymon Rojek <sz.rojek@gmail.com>
2020-08-04 14:31:13 +0100

    added 1 solution to the file 8-kyu/parse-float.js

--- 8-kyu/parse-float.js
+++ 8-kyu/parse-float.js
@@ -8,4 +8,13 @@ a number or Nothing (for Python None, for Javascript null) if conversion is not 
 */
 
 
+// 1: my solution
+
+const parseF = s => Number.isNaN(Number.parseFloat(s)) ? null : parseFloat(s);
+
+
+console.log(parseF('03')); // 3
+console.log(parseF('eee02')); // null
+console.log(parseF('1.3')); // 1.3
+console.log(parseF('2')); // 2
 
