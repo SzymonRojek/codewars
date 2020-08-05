02b200bbc046ce51b69fad8fa9e80645ab905140
Szymon Rojek <sz.rojek@gmail.com>
2020-08-05 23:05:47 +0100

    created new file in 8-kyu/string-repeat.js

---
+++ 8-kyu/string-repeat.js
@@ -0,0 +1,11 @@
+
+
+
+/*
+Description:
+Write a function called repeatString which repeats the given string src exactly count times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
