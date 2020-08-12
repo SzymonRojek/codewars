41c99cbf6e70abf37cc41f88d30bc3fe86896696
Szymon Rojek <sz.rojek@gmail.com>
2020-08-12 23:16:12 +0100

    added 1 solution to the file  7-kyu/simple-string-reversalII.js

--- 7-kyu/simple-string-reversalII.js
+++ 7-kyu/simple-string-reversalII.js
@@ -13,3 +13,25 @@ The first index a will always be lower that than the string length;
 the second index b can be greater than the string length. 
 More examples in the test cases. Good luck!
 */
+
+
+
+// 1:
+
+function solve(str, from, to){
+  const reverse = str.split('').splice(from, to - from + 1).reverse().join('');
+  
+  const cutStr  = [
+    str.substring(0, from),
+    str.substring(to + 1)
+  ];
+  
+  return cutStr[0] + reverse + cutStr[1];
+}
+
+
+console.log((solve("codewars",1,5))); // "cawedors"
+console.log((solve("codingIsFun",2,100))); // "conuFsIgnid"
+console.log((solve("FunctionalProgramming", 2, 15))); // "FuargorPlanoitcnmming"
+console.log((solve("abcdefghijklmnopqrstuvwxyz",0,20))); // "utsrqponmlkjihgfedcbavwxyz"
+console.log((solve("abcdefghijklmnopqrstuvwxyz",5,20))); // "abcdeutsrqponmlkjihgfvwxyz"
