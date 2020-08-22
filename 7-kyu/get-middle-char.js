0b3ab4c193a61bb6a1301f527c4b5d46a353bef5
Szymon Rojek <sz.rojek@gmail.com>
2020-08-22 22:16:59 +0100

    added 1 solution to the file 7-kyu/get-middle-char.js

--- 7-kyu/get-middle-char.js
+++ 7-kyu/get-middle-char.js
@@ -20,3 +20,27 @@ This is only here to tell you that you do not need to worry about your solution 
 
 The middle character(s) of the word represented as a string.
 */
+
+
+
+// 1
+
+
+function getMiddle(str) {
+
+  let position, length;
+  
+  if(str.length % 2 === 1) {
+      position = str.length / 2;
+      length = 1;  
+  } else {
+      position = str.length / 2 - 1;
+      length = 2;
+  }
+  
+  return str.substring(position, position + length);
+}
+console.log(getMiddle("test")); // 'es'
+console.log(getMiddle("testing")); // 't'
+console.log(getMiddle("middle")); // 'dd'
+console.log(getMiddle("A")); // 'A'
