a19f6d7ad8c6827d8ed4b217589b6a9484877f04
Szymon Rojek <sz.rojek@gmail.com>
2020-08-25 13:16:43 +0100

    added 1 solution to the 8-kyu/counting-sheep.js

--- 8-kyu/counting-sheep.js
+++ 8-kyu/counting-sheep.js
@@ -20,3 +20,26 @@ The correct answer would be 17.
 
 Hint: Don't forget to check for bad values like null/undefined
 */
+
+
+
+// 1
+
+function countSheeps(arrayOfSheep) {
+  
+  let countSheeps = 0;
+
+  arrayOfSheep.forEach(sheep => sheep === true? countSheeps++ : false);
+
+  
+  return `There are ${countSheeps} sheeps in total`; 
+}
+
+const array1 = [true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true ];
+
+console.log(countSheeps(array1)); // "There are 17 sheeps in total"
