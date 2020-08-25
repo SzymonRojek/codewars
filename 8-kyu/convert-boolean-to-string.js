a8b1c9019bf430b8ec6823ff44c1bdd684badcda
Szymon Rojek <sz.rojek@gmail.com>
2020-08-25 12:51:12 +0100

    added 1 solution to the 	8-kyu/convert-boolean-to-string.js

--- 8-kyu/convert-boolean-to-string.js
+++ 8-kyu/convert-boolean-to-string.js
@@ -6,3 +6,13 @@ Convert boolean values to strings 'Yes' or 'No'.
 
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 */
+
+
+// 1
+
+function boolToWord(bool) {
+  return bool === true ? 'Yes' : 'No';
+}
+
+console.log(boolToWord(true)); // 'Yes'
+console.log(boolToWord(false)); // 'No'
