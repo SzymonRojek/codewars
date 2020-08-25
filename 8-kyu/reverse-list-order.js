cbc72bf24b79ce132b80f2d1ab102e89c1a1795b
Szymon Rojek <sz.rojek@gmail.com>
2020-08-25 13:59:55 +0100

    added 1 solution to the  8-kyu/reverse-list-order.js

---
+++ 8-kyu/reverse-list-order.js
@@ -0,0 +1,30 @@
+
+
+
+
+/*
+
+Reverse list order:
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+As input you have got a string with digits and return array of digits. 
+
+*/
+
+function reverseList(list) {
+  
+  let reversedArr = [];
+
+  for (let i = list.length-1; i >= 0; i--) {
+    const num = list[i];
+
+    reversedArr.push(num);
+  }
+
+  return reversedArr;
+}
+
+console.log(reverseList([1,2,3,4])); // [4,3,2,1]
+console.log(reverseList([3,1,5,4])); // [4,5,1,3]
+ 
