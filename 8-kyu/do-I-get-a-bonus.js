1ed74f5f190513ab6a791470c2280b7c04532e83
Szymon Rojek <sz.rojek@gmail.com>
2020-08-27 23:56:02 +0100

    added 1 solution to the file 8-kyu/do-I-get-a-bonus.js

--- 8-kyu/do-I-get-a-bonus.js
+++ 8-kyu/do-I-get-a-bonus.js
@@ -15,3 +15,17 @@ Return the total figure the individual will receive as a string prefixed with "�
 (= "\u00A3", JS, Go, and Java), "$" (C#, C++, Ruby, Clojure, Elixir, PHP and Python, Haskell, Lua) or "¥" (Rust).
 
 */
+
+
+
+// 1
+
+const bonusTime = (salary, bonus) => bonus === true ? `£${salary * 10}` : `£${salary}`;
+
+console.log(bonusTime(10000, true)); // '£100000'
+console.log(bonusTime(25000, true)); // '£250000'
+console.log(bonusTime(10000, false)); // '£10000'
+console.log(bonusTime(60000, false)); // '£60000'
+console.log(bonusTime(2, true)); // '£20'
+console.log(bonusTime(78, false)); // '£78'
+console.log(bonusTime(67890, true)); // '£678900'
