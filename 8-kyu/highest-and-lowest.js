dc935cf3ff0d7d7b393a8423950f69e08d03ecda
Szymon Rojek <sz.rojek@gmail.com>
2020-08-27 23:27:16 +0100

    added 2 solution to the file 8-kyu/highest-and-lowest.js

--- 8-kyu/highest-and-lowest.js
+++ 8-kyu/highest-and-lowest.js
@@ -27,3 +27,13 @@ Output string must be two numbers separated by a single space, and highest numbe
 const highAndLow = num => [Math.max(...num.split(' ')), ' ', Math.min(...num.split(' '))].join('');
 
 console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")); // "542 -214"
+
+
+
+
+// 2
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
