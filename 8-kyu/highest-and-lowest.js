83ad14d13e7e02df9c299b5f11f7cc3460a04fdc
Szymon Rojek <sz.rojek@gmail.com>
2020-08-27 23:25:57 +0100

    added 1 solution to the file 8-kyu/highest-and-lowest.js

--- 8-kyu/highest-and-lowest.js
+++ 8-kyu/highest-and-lowest.js
@@ -3,6 +3,9 @@
 
 
 /*
+
+// Highest and Lowest
+
 In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 Example:
@@ -15,4 +18,12 @@ Notes:
 All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
+
 */
+
+
+// 1
+
+const highAndLow = num => [Math.max(...num.split(' ')), ' ', Math.min(...num.split(' '))].join('');
+
+console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")); // "542 -214"
