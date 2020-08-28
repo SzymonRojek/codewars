20965a9deea25d25a0b2632eac9416e545a39372
Szymon Rojek <sz.rojek@gmail.com>
2020-08-28 22:03:00 +0100

    created new file 6-kyu/create-phone-number.js

--- 6-kyu/create-phone-number.js
+++ 6-kyu/create-phone-number.js
@@ -32,3 +32,17 @@ function createPhoneNumber(numbers) {
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); // "(111) 111-1111"
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
+
+
+
+// 2
+
+function createPhoneNumber( numbers ) {
+  let phone = "(nnn) nnn-nnnn";
+
+  numbers.forEach(num => {
+    phone = phone.replace('n', num);
+  })
+
+  return phone;
+}
