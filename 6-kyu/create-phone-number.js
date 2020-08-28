05b8295f19c90ffa63888161810dacc31f4a8538
Szymon Rojek <sz.rojek@gmail.com>
2020-08-28 21:33:04 +0100

    added 1 solution to the file 6-kyu/create-phone-number.js

--- 6-kyu/create-phone-number.js
+++ 6-kyu/create-phone-number.js
@@ -4,6 +4,8 @@
 
 /*
 
+Create phone number
+
 Write a function that accepts an array of 10 integers (between 0 and 9), 
 that returns a string of those numbers in the form of a phone number.
 
@@ -13,3 +15,20 @@ The returned format must be correct in order to complete this challenge.
 Don't forget the space after the closing parentheses!
 
 */
+
+
+
+// 1
+
+function createPhoneNumber(numbers) {
+  const firstThreeNum = numbers.splice(0,3).join('');
+  const secondThreeNum = numbers.splice(0,3).join('');
+  const lastFourNum = numbers.join('');
+
+  return `(${firstThreeNum}) ${secondThreeNum}-${lastFourNum}`;
+}
+
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); // "(111) 111-1111"
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
