68696c2589bad7b510f37a3adc36e9726cf5764b
Szymon Rojek <sz.rojek@gmail.com>
2020-08-29 21:08:37 +0100

    added 1 solution to the file 8-kyu/reversed-words.js

--- 8-kyu/reversed-words.js
+++ 8-kyu/reversed-words.js
@@ -15,3 +15,30 @@ reverseWords("The greatest victory is that which requires no battle")
 // should return "battle no requires which that is victory greatest The"
 
 */
+
+
+
+// 1
+
+
+function reverseWords(str){
+
+  str = str.split(' ');
+
+  let reversed = [];
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    const word = str[i];
+    
+    reversed.push(word);
+  }
+
+  return reversed.join(' ');
+}
+
+
+console.log(reverseWords("hello world!")); // "world! hello"
+console.log(reverseWords("yoda doesn't speak like this" )); //  "this like speak doesn't yoda"
+console.log(reverseWords("foobar")); // "foobar"
+console.log(reverseWords("kata editor")); // "editor kata"
+console.log(reverseWords("row row row your boat")); // "boat your row row row"
