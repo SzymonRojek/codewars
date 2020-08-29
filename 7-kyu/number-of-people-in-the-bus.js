cb9f1a3f65dfd7dca6bc4208ef7b6faab6422bb3
Szymon Rojek <sz.rojek@gmail.com>
2020-08-29 20:50:07 +0100

    added 2 solution to the file 7-kyu/number-of-people-in-the-bus.js

--- 7-kyu/number-of-people-in-the-bus.js
+++ 7-kyu/number-of-people-in-the-bus.js
@@ -48,3 +48,21 @@ const number = function(busStops){
 console.log(number([[10,0],[3,5],[5,8]])); // 5
 console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])); // 17
 console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]])); // 21
+
+
+
+// 2
+
+const number = function(busStops) {
+  
+  let allPeople = 0;
+
+  for (let i = 0; i < busStops.length; i++) {
+    const el = busStops[i];
+
+    allPeople += el[0];
+    allPeople -= el[1];
+  }
+
+  return allPeople;
+}
