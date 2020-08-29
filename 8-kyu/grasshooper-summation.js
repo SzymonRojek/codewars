bbd3716e01b56e12c0aff77277d8ae860cf13763
Szymon Rojek <sz.rojek@gmail.com>
2020-08-29 21:13:46 +0100

    added 1 solution to the file 8-kyu/grasshooper-summation.js

--- 8-kyu/grasshooper-summation.js
+++ 8-kyu/grasshooper-summation.js
@@ -18,3 +18,24 @@ summation(8) -> 36
 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 
 */
+
+
+
+
+// 1
+
+const summation = num => {
+  
+  let sum = 0;
+
+  for (let i = 0; i < num; i++) {
+    const number = i+1;
+
+    sum +=number;
+  }
+ 
+  return sum;
+}
+ 
+console.log(summation(1)); // 1
+console.log(summation(8)); // 36
