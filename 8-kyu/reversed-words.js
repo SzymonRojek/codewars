5c205fd8df5cc48704b2a6465075df25d0a8021f
Szymon Rojek <sz.rojek@gmail.com>
2020-08-29 21:09:14 +0100

    added 2 solution to the file 8-kyu/reversed-words.js

--- 8-kyu/reversed-words.js
+++ 8-kyu/reversed-words.js
@@ -42,3 +42,12 @@ console.log(reverseWords("yoda doesn't speak like this" )); //  "this like speak
 console.log(reverseWords("foobar")); // "foobar"
 console.log(reverseWords("kata editor")); // "editor kata"
 console.log(reverseWords("row row row your boat")); // "boat your row row row"
+
+
+
+
+// 2
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
