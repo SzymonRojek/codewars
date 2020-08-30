f049d44c987fb1d6ab912053b24fcdc44a6e0a34
Szymon Rojek <sz.rojek@gmail.com>
2020-08-30 15:36:47 +0100

    added 3 solution to the file 8-kyu/sum-without-highest-and-lowest-number.js

--- 8-kyu/sum-without-highest-and-lowest-number.js
+++ 8-kyu/sum-without-highest-and-lowest-number.js
@@ -51,3 +51,27 @@ console.log(sumArray([ 6])); // 16
 // 2
 
 const sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((a, b) => a + b, 0) : 0;
+
+
+
+
+// 3
+
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    
+    array = array.sort(function(a,b) {return a - b;});
+
+    let total = 0;
+
+    for (let i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+
+    return total;
+  }
+}
