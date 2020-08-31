ac6bfc3d24b7065ff415e644adb3ec9e31c11994
Szymon Rojek <sz.rojek@gmail.com>
2020-08-31 23:00:49 +0100

    added 2 solution to the file 8-kyu/is-n-divisible-by-x-and-y.js

--- 8-kyu/is-n-divisible-by-x-and-y.js
+++ 8-kyu/is-n-divisible-by-x-and-y.js
@@ -23,9 +23,19 @@ Examples:
 
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0 ? true : false;
+  // return n % x === 0 && n % y === 0;
 }
 
 console.log(isDivisible(3,3,4)); // false
 console.log(isDivisible(12,3,4)); // true
 console.log(isDivisible(8,3,4)); // false
 console.log(isDivisible(48,3,4)); // true
+
+
+
+
+// 2
+
+function isDivisible(n, x, y) {
+  return !(n % x || n % y);
+}
