cf7a9abc94415f17bc72222189f26a9c8a8f8147
Szymon Rojek <sz.rojek@gmail.com>
2020-08-31 23:10:10 +0100

    added 1 solution to the file 8-kyu/jennys-secret-message.js

--- 8-kyu/jennys-secret-message.js
+++ 8-kyu/jennys-secret-message.js
@@ -13,3 +13,15 @@ She added a special case to her function, but she made a mistake.
 Can you help her?
 
 */
+
+
+
+// 1
+
+function greet(name){
+  return (name === 'Johnny') ? 'Hello, my love!' : `Hello, ${name}!`;
+}
+console.log(greet("Jim")); //  "Hello, Jim!"
+console.log(greet("Jane")); // "Hello, Jane!"
+console.log(greet("Simon")); // "Hello, Simon!"
+console.log(greet("Johnny")); // "Hello, my love!"
