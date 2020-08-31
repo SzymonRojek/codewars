fd6b5ebdbe2c5cdb0e1097209d9427077b42a414
Szymon Rojek <sz.rojek@gmail.com>
2020-08-31 23:14:48 +0100

    added 2 solution to the file 8-kyu/jennys-secret-message.js

--- 8-kyu/jennys-secret-message.js
+++ 8-kyu/jennys-secret-message.js
@@ -25,3 +25,12 @@ console.log(greet("Jim")); //  "Hello, Jim!"
 console.log(greet("Jane")); // "Hello, Jane!"
 console.log(greet("Simon")); // "Hello, Simon!"
 console.log(greet("Johnny")); // "Hello, my love!"
+
+
+
+
+// 2
+
+function greet(name){
+  return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+}
