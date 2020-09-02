f143b19d6f6d0af741b53424076c046b2271d76d
Szymon Rojek <sz.rojek@gmail.com>
2020-09-02 22:40:33 +0100

    added 2 solution to the file 8-kyu/calc-average.js

--- 8-kyu/calc-average.js
+++ 8-kyu/calc-average.js
@@ -20,3 +20,18 @@ const find_average = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
 console.log(find_average([1,1,1])); // 1
 console.log(find_average([1,2,3])); // 2
 
+
+
+
+// 2
+
+function find_average(array) {
+  let sum = 0;
+
+  for (i = 0; i < array.length; i++) {
+    
+    sum += array[i];
+  }
+
+  return sum / array.length;
+}
