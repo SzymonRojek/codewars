a6461ab4d8fdc5945338d4ea10979d84aa5ed5f8
Szymon Rojek <sz.rojek@gmail.com>
2020-09-02 22:37:51 +0100

    added 1 solution to the file 8-kyu/calc-average.js

--- 8-kyu/calc-average.js
+++ 8-kyu/calc-average.js
@@ -9,3 +9,14 @@ Calculate average
 Write function avg which calculates average of numbers in given list.
 
 */
+
+
+
+
+// 1
+
+const find_average = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
+
+console.log(find_average([1,1,1])); // 1
+console.log(find_average([1,2,3])); // 2
+
