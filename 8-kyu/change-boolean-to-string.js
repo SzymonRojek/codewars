517e91a0d74279c755747329b59de2e030129b11
Szymon Rojek <sz.rojek@gmail.com>
2020-09-02 22:50:53 +0100

    added 2 solution to the file 8-kyu/change-boolean-to-string.js

--- 8-kyu/change-boolean-to-string.js
+++ 8-kyu/change-boolean-to-string.js
@@ -16,9 +16,18 @@ Implement a function which convert the given boolean value into its string repre
 // 1
 
 function booleanToString(boolean) {
-  
+
   return String(boolean);
 }
 
 console.log(booleanToString(true)); // "true"
 console.log(booleanToString(false)); // "false"
+
+
+
+
+// 2
+
+function booleanToString(b) {
+  return b ? 'true' : 'false';
+}
