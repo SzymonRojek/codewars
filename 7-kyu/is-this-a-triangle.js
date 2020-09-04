5c0671ef5e8510a2ff018c2155fa5102fa85d34e
Szymon Rojek <sz.rojek@gmail.com>
2020-09-04 21:40:32 +0100

    added 1 solution to the file 7-kyu/is-this-a-triangle.js

--- 7-kyu/is-this-a-triangle.js
+++ 7-kyu/is-this-a-triangle.js
@@ -13,3 +13,28 @@ The method should return true if a triangle can be built with the sides of given
 (In this case, all triangles must have surface greater than 0 to be accepted).
 
 */
+
+
+
+// 1
+
+function isTriangle(a, b, c) {
+  a = Number(a);
+  b = Number(b);
+  c = Number(c);
+
+      if(isNaN(a) || isNaN(b) || isNaN(c)) {
+        throw 'Argument is not a number!';
+      }
+      if(Math.min(a, b, c) < 0) {
+        throw 'Argument has to be positive';
+      }
+      const max = Math.max(a, b, c);
+  return a + b + c - max > max;
+}
+
+
+console.log(isTriangle('3','2','2')); // true
+console.log(isTriangle('1','1','2')); // false
+console.log(isTriangle(-1,5,3)); // argument has to be positive
+console.log(isTriangle(5,5,'eee')); // argument is not a number
