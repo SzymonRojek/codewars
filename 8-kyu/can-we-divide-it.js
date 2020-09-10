250d09f16b59345732abd36640ff2c86aeeeb84f
Szymon Rojek <sz.rojek@gmail.com>
2020-09-10 11:09:15 +0100

    created new file 8-kyu/can-we-divide-it.js

---
+++ 8-kyu/can-we-divide-it.js
@@ -0,0 +1,12 @@
+
+
+
+
+/*
+Can we divide it?
+
+Description:
+Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
+
+A few cases:
+*/
