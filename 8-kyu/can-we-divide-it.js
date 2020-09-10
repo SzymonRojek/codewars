ba6cfc2c925600d51e98fe32722288652a8e2fb5
Szymon Rojek <sz.rojek@gmail.com>
2020-09-10 11:10:46 +0100

    added 2 solution to the file 8-kyu/can-we-divide-it.js

--- 8-kyu/can-we-divide-it.js
+++ 8-kyu/can-we-divide-it.js
@@ -26,3 +26,9 @@ console.log(isDivideBy(45, 1, 6)); // false
 console.log(isDivideBy(45, 5, 15)); // true
 console.log(isDivideBy(4, 1, 4)); // true
 console.log(isDivideBy(15, -5, 3)); //  true
+
+
+
+// 2
+
+const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
