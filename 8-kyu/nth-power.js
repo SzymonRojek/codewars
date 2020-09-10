1d65f2160355e33a314da22e65da2ae59c91e479
Szymon Rojek <sz.rojek@gmail.com>
2020-09-10 18:35:41 +0100

    added 1 solution to the file 8-kyu/nth-power.js

--- 8-kyu/nth-power.js
+++ 8-kyu/nth-power.js
@@ -19,3 +19,26 @@ array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 
 */
+
+
+
+// 1
+
+function index(array, n){
+  
+  if(n > array.length) return -1;
+
+  return n < array.length ? Math.pow(array[n], n) : -1
+}
+
+
+console.log(index([1, 2, 3, 4],2)); // 9
+console.log(index([1, 3, 10, 100],3)); // 1000000
+console.log(index([0, 1],0)); // 1
+console.log(index([1, 2],3)); // -1
+console.log(index([0], 0)); // 1
+console.log(index([1,1,1,1,1,1,1,1,1,1], 9)); // 1
+console.log(index([1,1,1,1,1,1,1,1,1,2], 9)); // 512
+console.log(index([29,82,45,10], 3)); // 1000
+console.log(index([6,31], 3)); // -1
+console.log(index([75,68,35,61,9,36,89,0,30], 10)); // -1
