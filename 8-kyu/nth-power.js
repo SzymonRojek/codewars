8add42e58d440aa36884fa248447adea542d20e5
Szymon Rojek <sz.rojek@gmail.com>
2020-09-10 18:36:09 +0100

    added 2 solution to the file 8-kyu/nth-power.js

--- 8-kyu/nth-power.js
+++ 8-kyu/nth-power.js
@@ -42,3 +42,9 @@ console.log(index([1,1,1,1,1,1,1,1,1,2], 9)); // 512
 console.log(index([29,82,45,10], 3)); // 1000
 console.log(index([6,31], 3)); // -1
 console.log(index([75,68,35,61,9,36,89,0,30], 10)); // -1
+
+
+
+// 2
+
+const index = (array, n) => n < array.length ? Math.pow(array[n], n) : -1;
