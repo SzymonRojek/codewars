1c78c0f55d8b35cb5b6a27e55a36fd9bff6cb6c9
Szymon Rojek <sz.rojek@gmail.com>
2020-09-10 11:10:20 +0100

    added 1 solution to the file 8-kyu/can-we-divide-it.js

--- 8-kyu/can-we-divide-it.js
+++ 8-kyu/can-we-divide-it.js
@@ -10,3 +10,19 @@ Your task is to create functionisDivideBy (or is_divide_by) to check if an integ
 
 A few cases:
 */
+
+
+
+// 1
+
+function isDivideBy(number, a, b) {
+
+  return Number.isInteger(number / a) && Number.isInteger(number / b);
+}
+
+console.log(isDivideBy(-12, 2, -6)); // true
+console.log(isDivideBy(-12, 2, -5)); // false
+console.log(isDivideBy(45, 1, 6)); // false
+console.log(isDivideBy(45, 5, 15)); // true
+console.log(isDivideBy(4, 1, 4)); // true
+console.log(isDivideBy(15, -5, 3)); //  true
