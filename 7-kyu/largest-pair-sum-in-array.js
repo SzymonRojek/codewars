bc32fe551d4303819ef30074afbb9d00cb6b62da
Szymon Rojek <sz.rojek@gmail.com>
2020-09-13 20:58:37 +0100

    created 7-kyu/largest-pair-sum-in-array.js

---
+++ 7-kyu/largest-pair-sum-in-array.js
@@ -0,0 +1,18 @@
+
+
+
+
+/*
+
+Largest pair sum in an array
+
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+Input sequence contains minimum two elements and every element is an integer.
+
+*/
