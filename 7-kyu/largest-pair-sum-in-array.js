6c1b6f4a382959a9058ddafdaa65f5b27e4dbc0b
Szymon Rojek <sz.rojek@gmail.com>
2020-09-13 21:00:49 +0100

    added 2 solution to the 7-kyu/largest-pair-sum-in-array.js

--- 7-kyu/largest-pair-sum-in-array.js
+++ 7-kyu/largest-pair-sum-in-array.js
@@ -27,3 +27,13 @@ const largestPairSum = num =>  num.map(v => num.sort((a, b) => b - a)) ? num[0] 
 console.log(largestPairSum([-10, -8, -16, -18, -19])); // -18
 console.log(largestPairSum([10, 14, 2, 23, 19])); // -->  42 (= 23 + 19)
 console.log(largestPairSum([99, 2, 2, 23, 19])); //  --> 122 (= 99 + 23)
+
+
+
+
+// 2
+
+function largestPairSum(numbers) {
+  let [a, b] = numbers.sort((a, b) => b - a);
+  return a + b;
+}
