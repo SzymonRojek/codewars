7c6c1f9eb14b8e0a30325528d4d14eecc3d81990
Szymon Rojek <sz.rojek@gmail.com>
2020-09-14 10:36:01 +0100

    added 5 solution to the 7-kyu/is-isogram.js

--- 7-kyu/is-isogram.js
+++ 7-kyu/is-isogram.js
@@ -64,3 +64,19 @@ function isIsogram(str){
  
   return [...str.toLowerCase()].filter((v, i, arr) => arr.indexOf(v) === i).length == str.length;
 }
+
+
+
+// 5
+
+function isIsogram(str) {
+  str = str.toLowerCase();
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str.charAt(i), i + 1) >= 0) {
+      return false;
+    }
+  }
+  
+  return true;
+}
