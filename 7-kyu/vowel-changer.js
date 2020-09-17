dbd97fb6f3a4df0389fb0bff1706dc0d4910380a
Szymon Rojek <sz.rojek@gmail.com>
2020-09-17 22:44:01 +0200

    added 1 solution to the 7-kyu/vowel-changer.js

---
+++ 7-kyu/vowel-changer.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+/*
+Vowel changer
+
+Create a function that changes all the vowels (excluding y) in a string, and changes them all to the same vowel. The first parameter of the function is the string, and the second is the vowel that all the vowels in the string are being changed to.
+
+For Example:
+
+  vowelChange("Hannah Hannah bo-bannah Banana fanna fo-fannah Fee, fy, mo-mannah. Hannah!",'i') // => 'Hinnih Hinnih bi-binnih Binini finni fi-finnih Fii, fy, mi-minnih. Hinnih!'
+  vowelChange('adira wants to go to the park', 'o') // =>'odoro wonts to go to tho pork'
+*/ 
+
+
+
+
+// 1
+
+const vowelChange = (str, vow) => str.replace(/[aeiou]/gi, vow)
+
+
+console.log(vowelChange("Hannah Hannah bo-bannah Banana fanna fo-fannah Fee, fy, mo-mannah. Hannah!",'i'));
+// 'Hinnih Hinnih bi-binnih Binini finni fi-finnih Fii, fy, mi-minnih. Hinnih!'
+
+console.log(vowelChange('adira wants to go to the park', 'o'));
+// 'odoro wonts to go to tho pork'
+
+console.log(vowelChange('amigo', 'o')); // omogo
+
+
