cb89ac2daa5186c599f9bac64c7fe066def58fec
Szymon Rojek <sz.rojek@gmail.com>
2020-10-11 20:42:03 +0100

    added 1 solution to the file 7-kyu/16+18=214.js

--- 7-kyu/16+18=214.js
+++ 7-kyu/16+18=214.js
@@ -13,3 +13,32 @@ For example:
 4416
 
 */
+
+
+// 1
+
+function add(num1, num2) {
+  const arr1 = [...String(num1)].map(Number).reverse();
+  const arr2 = [...String(num2)].map(Number).reverse();
+  
+  let result = [];
+
+  for (let i = 0; i < Math.max(arr.length, arr2.length); i++) {
+
+    result.push((arr1[i] || 0) + (arr2[i] || 0));
+  }
+
+
+  return +result.reverse().join('');
+}
+console.log(add(122, 81)); //  1103
+console.log(add(248, 208)); // 4416
+console.log(add(2, 11)); // 13
+console.log(add(0, 1)); // 1
+console.log(add(0, 0)); // 0
+console.log(add(16, 18)); // 214
+console.log(add(26, 39)); //  515
+console.log(add(1222, 30277)); //  31499
+console.log(add(1236, 30977)); // 31111013
+console.log(add(38810, 1383)); // 391193
+console.log(add(49999, 49999)); //  818181818
