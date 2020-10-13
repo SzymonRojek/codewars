907420344e3a5e2f07078e06bf44565dc3ac9ba1
Szymon Rojek <sz.rojek@gmail.com>
2020-10-13 18:14:57 +0100

    added 1 solution to the file 7-kyu/hungarian-vowel-harmony.js

--- 7-kyu/hungarian-vowel-harmony.js
+++ 7-kyu/hungarian-vowel-harmony.js
@@ -20,3 +20,33 @@ dative("ablak") == "ablaknak"
 dative("szék") == "széknek"
 dative("otthon") == "otthonnak"
 */
+
+
+// 1
+
+function dative(word) {
+  const frontVowel = ['e', 'é', 'i', 'í', 'ö', 'ő', 'ü', 'ű'] // nek
+  const backVowel = ['a', 'á', 'o', 'ó', 'u', 'ú'] // nak
+  let newWord = ''
+ 
+  word.split('').forEach( letter => {
+    if(frontVowel.includes(letter)) {
+      newWord = word + 'nek'
+    }
+    if (backVowel.includes(letter)) {
+      newWord = word + 'nak'
+    }
+  })
+
+ return newWord
+}
+
+console.log(dative("ablak")); // "ablaknak"
+console.log(dative("tükör")); // "tükörnek"
+console.log(dative("keret")); // "keretnek"
+console.log(dative("otthon")); // "otthonnak"
+console.log(dative("virág")); // "virágnak"
+console.log(dative("tett")); // "tettnek"
+console.log(dative("rokkant")); // "rokkantnak"
+console.log(dative("rossz")); // "rossznak"
+console.log(dative("gonosz")); // "gonosznak"
