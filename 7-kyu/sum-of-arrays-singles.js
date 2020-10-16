177e4309d6ef1f19cde701a01b28f94ad6925c62
Szymon Rojek <sz.rojek@gmail.com>
2020-10-16 21:12:39 +0100

    added 1 solution to the file  7-kyu/sum-of-arrays-singles.js

--- 7-kyu/sum-of-arrays-singles.js
+++ 7-kyu/sum-of-arrays-singles.js
@@ -12,3 +12,31 @@ More examples in the test cases.
 Good luck!
 
 */
+
+
+// 1
+
+function noRepeats(arr) {
+  let singleVal = []
+
+  let obj = arr.reduce((acc, val) => {
+    acc[val] ? acc[val]++ : acc[val] = 1;
+    return acc
+  } , {})
+
+    for (const key in obj) {
+      const value = obj[key]
+        if(value === 1) {
+          singleVal.push(Number(key))
+        }
+    }
+
+return singleVal;
+}
+
+console.log(noRepeats([4,5,7,5,4,8])); // 15
+console.log(repeats([9, 10, 19, 13, 19, 13])); // 19
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11])); // 12
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13])); // 22
+console.log(repeats([5, 10, 19, 13, 10, 13])); // 24
+
