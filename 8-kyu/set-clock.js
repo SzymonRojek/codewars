48581714e0649f9a918368ad60e0ba38613cf3e2
Szymon Rojek <sz.rojek@gmail.com>
2020-10-17 22:02:16 +0100

    added 3 solution to the file 8-kyu/set-clock.js

--- 8-kyu/set-clock.js
+++ 8-kyu/set-clock.js
@@ -40,3 +40,14 @@ function past(h, m, s) {
 
   return ((h * 3600) + (m * 60) + s) * 1000;
 }
+
+
+
+// 3
+
+function past(h, m, s){
+  const setTime = new Date().setHours(h, m, s);
+  const midnight = new Date().setHours(0, 0, 0);
+  
+  return Math.round(setTime - midnight);
+}
